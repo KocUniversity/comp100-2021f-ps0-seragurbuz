46f40403155e405ac89c59481a42bacdd4ae1ee2
seragurbuz <91701182+seragurbuz@users.noreply.github.com>
2021-10-01 10:08:29 +0000

    I completed the assignment by following the steps, got the numbers x and y from the user, printed out x raised to the power y and then the log base 2 of x, finally I got the student ID from the user and printed it out. My student ID number is 76234.

---
+++ main.py
@@ -0,0 +1,10 @@
+x = int(input('Enter a number x:'))
+y = int(input('Enter a number y:'))
+#print out x raised to the power y
+print(x**y)
+#print out the log(base 2) of x
+import numpy
+print(numpy.log2(x))
+#print student ID number
+idnum = input ('Enter your student ID number:')
+print('Student ID number is '+ idnum)
